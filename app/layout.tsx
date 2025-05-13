2c90c28402986248fe66465a603572ac09ce6c56
kgaurav152 <kgaurav152@gmail.com>
2025-05-13 16:42:13 +0530

    added theme provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Provider from "./provider";
 import AuthWrapper from "./_components/AuthWrapper";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,16 +27,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark  text-white`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>
-          <AuthWrapper>
-            {children}
-            <Toaster />
-          </AuthWrapper>
-        </Provider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Provider>
+            <AuthWrapper>
+              {children}
+              <Toaster position="top-right" richColors />
+            </AuthWrapper>
+          </Provider>
+        </ThemeProvider>
       </body>
     </html>
   );
